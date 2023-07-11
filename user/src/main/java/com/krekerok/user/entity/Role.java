9c735b535238cb327c336d77f7fdbb61ff8cc044
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-07-11 15:30:07 +0300

    Added base classes

---
+++ user/src/main/java/com/krekerok/user/entity/Role.java
@@ -0,0 +1,6 @@
+package com.krekerok.user.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ user/src/main/java/com/krekerok/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.krekerok.user.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@ToString
+@Table(name = "users")
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usersSeqGenerator")
+    @SequenceGenerator(name = "usersSeqGenerator", sequenceName = "users_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "localization")
+    private String localization;
+}
