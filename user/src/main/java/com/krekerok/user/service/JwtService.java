9ab282b59dee51577709f1f9219f29c1ebe78cb7
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-07-12 15:52:21 +0300

    Added new logic

---
+++ user/src/main/java/com/krekerok/user/service/JwtService.java
@@ -0,0 +1,17 @@
+package com.krekerok.user.service;
+
+import com.krekerok.user.entity.User;
+import io.jsonwebtoken.Claims;
+import java.util.function.Function;
+
+public interface JwtService {
+
+    String extractUsername(String token);
+    <T> T extractClaim(String token, Function<Claims, T> claimsResolver);
+
+    String generateAccessToken(User user);
+
+    String generateRefreshToken(User userDetails);
+
+    boolean isTokenValid(String token, User user);
+}
---
+++ user/src/main/java/com/krekerok/user/service/impl/JwtServiceImpl.java
@@ -0,0 +1,91 @@
+package com.krekerok.user.service.impl;
+
+import com.krekerok.user.entity.User;
+import com.krekerok.user.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    @Value("${security.jwt.secret}")
+    private String SECRET_KEY ;
+    @Value("${security.jwt.token.validity.access}")
+    private long ACCESS_TOKEN_VALIDITY;
+    @Value("${security.jwt.token.validity.refresh}")
+    private long REFRESH_TOKEN_VALIDITY;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateAccessToken(User user) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", user.getRole());
+        return Jwts.builder()
+            .setClaims(extraClaims)
+            .setSubject(user.getEmail())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(ACCESS_TOKEN_VALIDITY)))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .compact();
+
+    }
+
+    public String generateRefreshToken(User userDetails) {
+        return Jwts.builder()
+            .setSubject(userDetails.getEmail())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(REFRESH_TOKEN_VALIDITY)))
+            .signWith(getRefreshTokenSigningKey(), SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    private Key getRefreshTokenSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        final String username = extractUsername(token);
+        return (username.equals(user.getEmail())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
