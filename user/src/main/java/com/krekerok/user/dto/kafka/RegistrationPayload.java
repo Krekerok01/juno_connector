b78b2614d92255becfcd50f44392fc23ab31f11f
Varvara <65535951+Krekerok01@users.noreply.github.com>
2023-07-18 16:56:50 +0300

    [Feature]#9: Added Kafka to the project. Connected user-service with kafka

---
+++ user/src/main/java/com/krekerok/user/dto/kafka/RegistrationPayload.java
@@ -0,0 +1,20 @@
+package com.krekerok.user.dto.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationPayload {
+
+    private String username;
+    private String role;
+}
