53312340e273ae7b07c0ca2dedd483086e67c640
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-08-07 19:47:42 +0300

    Added entity class

---
+++ forum/src/main/java/com/krekerok/forum/entity/Question.java
@@ -0,0 +1,53 @@
+package com.krekerok.forum.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "questions")
+@Setter
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "questionsSeqGenerator")
+    @SequenceGenerator(name = "questionsSeqGenerator", sequenceName = "questions_seq", allocationSize = 1)
+    @Column(name = "question_id")
+    private Long questionId;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "is_open", nullable = false)
+    private boolean isOpen;
+
+    @Column(name = "question_text", nullable = false)
+    private String questionText;
+
+    @Column(name = "opening_date")
+    private LocalDateTime openingDate;
+
+    @Column(name = "closing_date")
+    private LocalDateTime closingDate;
+
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+}
