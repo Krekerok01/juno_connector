9d366e6806f543f7859649b40b19f8b88087571b
Varvara Mamatsiuk <matyush2001@gmail.com>
2023-08-30 11:32:46 +0300

    Added repositories for entities

---
+++ profile/src/main/java/com/krekerok/profile/repository/JobRepository.java
@@ -0,0 +1,9 @@
+package com.krekerok.profile.repository;
+
+import com.krekerok.profile.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
